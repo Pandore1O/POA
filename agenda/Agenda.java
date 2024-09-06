30c2b0503c189fa2c800634e55657d04b10aaa79
Pandore1O <143713423+Pandore1O@users.noreply.github.com>
2024-09-06 00:38:49 -0300

    Add files via upload

---
+++ agenda/Agenda.java
@@ -0,0 +1,86 @@
+package com.menezes.poa.agenda;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+
+public class Agenda {
+	
+	public void escrita(String nome, String telefone) {
+		
+		try {
+			FileWriter fileWriter = new FileWriter("agenda.txt", true);
+			BufferedWriter bufferedWriter = new BufferedWriter(fileWriter);
+			
+			bufferedWriter.write(nome + ";" + telefone);
+			bufferedWriter.newLine();
+			bufferedWriter.close();
+			fileWriter.close();
+		} catch(IOException e) {
+			e.printStackTrace();
+		}
+	}
+	
+	public void acharTodos() {
+		FileReader fileReader;
+		try {
+			fileReader = new FileReader("agenda.txt");
+			BufferedReader bufferedReader = new BufferedReader(fileReader);
+			
+			while (bufferedReader.ready()) {
+				String aux = bufferedReader.readLine();
+				String[] dados = aux.split(";");
+				System.out.println("Nome: " + dados[0] + "\nTelefone: " + dados[1]);
+			}
+			
+			bufferedReader.close();
+			fileReader.close();
+			
+		} catch (FileNotFoundException e) {
+			e.printStackTrace();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		
+	}
+	
+	public void procura(String nome) {
+		FileReader fileReader;
+		try {
+			fileReader = new FileReader("agenda.txt");
+			BufferedReader bufferedReader = new BufferedReader(fileReader);
+			
+			boolean encontrado = false;
+			
+			while (bufferedReader.ready()) {
+				String aux = bufferedReader.readLine();
+				
+				if (aux.toLowerCase().contains(nome.toLowerCase())) {
+					String[] dados = aux.split(";");
+					System.out.println("Nome: " + dados[0] + "\nTelefone: " + dados[1]);
+					encontrado = true;
+				}
+				
+				if (encontrado) {
+					bufferedReader.close();
+					fileReader.close();
+					System.out.println("Encontrado.");
+					break;
+				}
+				
+			}
+			
+			bufferedReader.close();
+			fileReader.close();
+			
+		} catch (FileNotFoundException e) {
+			e.printStackTrace();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		
+	}
+}
---
+++ agenda/Main.java
@@ -0,0 +1,15 @@
+package com.menezes.poa.agenda;
+
+public class Main {
+
+	public static void main(String[] args) {
+		
+		Agenda ag = new Agenda();
+		
+		ag.escrita("Felipe", "99999");
+		ag.escrita("João", "88888");
+		ag.acharTodos();
+
+	}
+
+}
