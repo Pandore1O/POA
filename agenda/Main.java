05f77cdc5254074d56e287a1b9da720944a83c25
Pandore1O <143713423+Pandore1O@users.noreply.github.com>
2024-09-06 11:49:42 -0300

    Update Main.java

--- agenda/Main.java
+++ agenda/Main.java
@@ -1,4 +1,4 @@
-package com.menezes.poa.agenda;
+package Agenda;
 
 public class Main {
 
@@ -8,7 +8,13 @@ public class Main {
 		
 		ag.escrita("Felipe", "99999");
 		ag.escrita("João", "88888");
+		ag.escrita("LumahBoboca", "11111");
+		
+		System.out.println("Achar todos\n");
 		ag.acharTodos();
+		
+		System.out.println("\nProcurar alguém especifico\n");
+		ag.procura("Lumah");
 
 	}
 
