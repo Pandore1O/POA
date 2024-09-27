f1db2757a18f67d214140e6d4fe9b06ca127cd59
Pandore1O <143713423+Pandore1O@users.noreply.github.com>
2024-09-27 09:25:03 -0300

    Update ModificaServlet.java

--- PrimeiroAppWeb/src/main/java/servlet/ModificaServlet.java
+++ PrimeiroAppWeb/src/main/java/servlet/ModificaServlet.java
@@ -34,8 +34,48 @@ public class ModificaServlet extends HttpServlet {
 	 */
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		String texto = request.getParameter("texto");
-		texto = texto.replace("a", "*");
 		HttpSession session = request.getSession();
+		
+		int resultado = 0;
+		
+		resultado = (texto.equalsIgnoreCase("Lumah")) ? 1 : resultado;
+		resultado = (texto.equalsIgnoreCase("Felipe")) ? 2 : resultado;
+		resultado = (texto.equalsIgnoreCase("Amanda")) ? 3 : resultado;
+		resultado = (texto.equalsIgnoreCase("Lucas")) ? 4 : resultado;
+		resultado = (texto.equalsIgnoreCase("Miguel")) ? 5 : resultado;
+		resultado = (texto.equalsIgnoreCase("cauÃ£")) ? 6 : resultado;
+		
+		switch (resultado) {
+		
+		case 1:
+			texto = texto.replace(texto, "Sai daqui doida.");
+			break;
+			
+		case 2:
+			texto = texto.replace(texto, "O criador.");
+			break;
+			
+		case 3:
+			texto = texto.replace(texto, "Herbella... Parece um bom nome pra uma marca de roupa de luxo. lel");
+			break;
+			
+		case 4:
+			texto = texto.replace(texto, "Lucas... É meio ZaaZ. éverdade esse bilhete.");
+			break;
+		
+		case 5:
+			texto = texto.replace(texto, "Miguel... O professor mais agitado e melhor da FAETERJ.");
+			break;
+			
+		case 6:
+			texto = texto.replace(texto, "VASCO.");
+			break;
+			
+		default:
+			texto = texto.replace("a", "*");
+			break;
+		}
+		
 		session.setAttribute("texto", texto);
 		
 		response.sendRedirect("index.jsp");
